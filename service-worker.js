78e308cc7bcae66c37ff8f6656eea797c1a8d8a7
DM <147567713+DoryanXS@users.noreply.github.com>
2024-02-26 16:10:57 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,8 @@
-const CACHE_NAME = 'Majestic-Alliance';
-const urlsToCache = [
-    './',
-    './index.html',
-    './styles/main.css',
-    './scripts/main.js',
-    './images/icon-144.png',
-    './images/icon-192.png',
-    './images/icon-196.png',
-    './images/icon-512.png'
-];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-            .then(() => console.log('Cache installation successful'))
-            .catch(error => console.error('Cache installation failed:', error))
-    );
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetchAndCache(event.request))
-    );
-});
-
 function fetchAndCache(request) {
+    if (request.url.startsWith('chrome-extension://')) {
+        return fetch(request);
+    }
+
     return fetch(request)
         .then(response => {
             if (!response || response.status !== 200 || response.type !== 'basic') {
@@ -51,20 +27,3 @@ function fetchAndCache(request) {
             return response;
         });
 }
-
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-
-    event.waitUntil(
-        caches.keys()
-            .then(cacheNames => Promise.all(
-                cacheNames.map(cacheName => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            ))
-            .then(() => console.log('Cache cleanup successful'))
-            .catch(error => console.error('Cache cleanup failed:', error))
-    );
-});
