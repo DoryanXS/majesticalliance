de20796cf0587029071613b28200b154b39f2c87
DoryanU <147567713+DoryanU@users.noreply.github.com>
2024-06-16 10:35:41 -0700

    Nuevos Atributos
    Cambio de atributos de legibilidad.

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 // Main Alerts \\
-function NotAvailable() {
+function NotAvailable()
+{
     return alert("¡Esta sección estará disponible próximamente!");
 }
-function NotWork() {
+
+function NotWork()
+{
     return alert("¡Actualmente esta sección no se encuentra disponible!");
 }
 
 // Game Alerts \\
-function Spotifyless() {
+function Spotifyless()
+{
     return alert("¡Este videojuego no cuenta con el SoundTrack Origal Disponible!");
 }
-function Nintendoless() {
+
+function Nintendoless()
+{
     return alert("¡Este videojuego no se encuentra disponible en la tienda de Nintendo!");
 }
-function Xboxless() {
+
+function Xboxless()
+{
     return alert("¡Este videojuego no se encuentra disponible en la tienda de Xbox!");
 }
-function PCless() {
+
+function PCless()
+{
     return alert("¡Este videojuego no se encuentra disponible en la tienda de Xbox!");
 }
